061ee15ab7ffab4ced5a7338086345434afe665d
EDUARDO SILVA <educwb@gmail.com>
2021-10-12 13:21:20 -0300

    adicionado UserDetails ate 19:25

---
+++ src/components/userDetails/index.jsx
@@ -0,0 +1,16 @@
+import { 
+    Container,
+    Name,
+    Username,
+    Description
+} from './styles'
+
+const UserDetails = props => (
+    <Container>
+        <Name>{props.name}</Name>
+        <Username>{props.login}</Username>
+        <Description>{props?.bio}</Description>
+    </Container>
+)
+
+export default UserDetails
