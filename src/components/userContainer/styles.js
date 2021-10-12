f07ed0f4f4c47d838ec4bdfc22a7a6481a08ccd3
EDUARDO SILVA <educwb@gmail.com>
2021-10-12 15:55:05 -0300

    adicionado userNumbers ate 26:32

--- src/components/userContainer/styles.js
+++ src/components/userContainer/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
     width: 400px;
-    height: 300px;
+    /* height: 300px; // retirado para UserNumbers fique dentro */
     background: rgba(255,255,255,.95);
     border-radius: 10px;
     border-bottom: 10px solid #FF7A00;
---
+++ src/components/userNumbers/index.jsx
@@ -0,0 +1,23 @@
+import {
+    Container,
+    NumberContainer
+} from './styles'
+
+const UserNumbers = props => (
+    <Container>
+        <NumberContainer>
+            <h1>15</h1>
+            <h2>Repositórios</h2>
+        </NumberContainer>
+        <NumberContainer>
+            <h1>15</h1>
+            <h2>Seguidores</h2>
+        </NumberContainer>
+        <NumberContainer>
+            <h1>15</h1>
+            <h2>Seguindo</h2>
+        </NumberContainer>
+    </Container>
+)
+
+export default UserNumbers
